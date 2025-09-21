cdba07305746403ad8b3ab6f27e211f0133eb51f
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2025-09-21 12:14:08 -0400

    Add files via upload

---
+++ R/rgZ_datasource.R
@@ -0,0 +1,7 @@
+#' Genetic correlation Z matrix for 15 diseases
+#'
+#' Example genetic correlation Z matrix used in the vignette and examples.
+#'
+#' @format A numeric matrix.
+#' @source Internal simulation
+"Rg_mat_z_15D"
---
+++ R/rgarray_datasource.R
@@ -0,0 +1,5 @@
+#' Jackknife array of genetic correlations (15 diseases)
+#'
+#' @format A 3-dim array.
+#' @source Internal simulation
+"Results_full_rg_array_15D"
