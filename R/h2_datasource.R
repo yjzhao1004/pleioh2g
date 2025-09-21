f45908140aca2025dde7c0301a48b6bbf48a4824
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2025-09-20 20:57:08 -0400

    Delete R directory

--- R/h2_datasource.R
+++
@@ -1,7 +0,0 @@
-#' h2 vector for 15 diseases
-#'
-#' Example h2 vector used in the vignette and examples.
-#'
-#' @format A numeric matrix of size 15 × 1.
-#' @source Internal simulation
-"h2_vector_15D"
