a2b1d494c42724a5e946597b15cd701b5420f2e5
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2025-09-21 12:12:47 -0400

    Delete R directory

--- R/globals.R
+++
@@ -1,9 +0,0 @@
-## File: R/globals.R
-## Purpose: Declare global variables to silence R CMD check
-##          “no visible binding for global variable …” warnings
-
-utils::globalVariables(c(
-  ".data","N", "SNP", "Z", "A1", "CHR", "h2_Z",
-  "phenotype_path_update", "save_path", ".x"
-))
-
