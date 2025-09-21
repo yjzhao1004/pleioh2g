02606c6f4b496aa22365945f199087c4f5c5f0a2
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2025-09-20 21:08:40 -0400

    Delete R directory

--- R/rgZ_datasource.R
+++
@@ -1,7 +0,0 @@
-#' Genetic correlation Z matrix for 15 diseases
-#'
-#' Example genetic correlation Z matrix used in the vignette and examples.
-#'
-#' @format A numeric matrix of size 15 × 15.
-#' @source Internal simulation
-"Rg_mat_z_15D"
--- R/rg_datasource.R
+++
@@ -1,7 +0,0 @@
-#' Genetic correlation matrix for 15 diseases
-#'
-#' Example genetic correlation matrix used in the vignette and examples.
-#'
-#' @format A numeric matrix of size 15 × 15.
-#' @source Internal simulation
-"Results_full_rg_15D"
