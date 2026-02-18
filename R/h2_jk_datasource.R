cb9c27e1a8fe8975e2853b59783b463a51c00672
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2026-02-18 20:06:36 +0800

    Delete R directory

--- R/h2_jk_datasource.R
+++
@@ -1,7 +0,0 @@
-#' h2 jk matrix for 15 diseases
-#'
-#' Example h2 jk matrix used in the vignette and examples.
-#'
-#' @format A numeric matrix.
-#' @source Internal simulation
-"h2_vector_mat_15D"
