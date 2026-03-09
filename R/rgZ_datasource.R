3d6a7ec25881cf48bf49cc017da5175cc133c36c
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2026-03-09 21:26:21 +0800

    Delete R directory

--- R/rgZ_datasource.R
+++
@@ -1,7 +0,0 @@
-#' Genetic correlation Z matrix for 62 traits
-#'
-#' Example genetic correlation Z matrix used in the vignette and examples.
-#'
-#' @format A numeric matrix.
-#' @source Internal simulation
-"Rg_mat_z"
