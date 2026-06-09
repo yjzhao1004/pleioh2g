e33dfd064a682b4f640743d57cca999752648b50
yjzhao1004 <123579364+yjzhao1004@users.noreply.github.com>
2026-06-09 16:13:21 +0800

    Delete R directory

--- R/h2_datasource.R
+++
@@ -1,7 +0,0 @@
-#' h2 vector for 62 traits
-#'
-#' Example h2 vector used in the vignette and examples.
-#'
-#' @format A numeric matrix.
-#' @source Internal simulation
-"h2_vector"
--- R/rg_datasource.R
+++
@@ -1,7 +0,0 @@
-#' Genetic correlation matrix for 62 traits
-#'
-#' Example genetic correlation matrix used in the vignette and examples.
-#'
-#' @format A numeric matrix.
-#' @source Internal simulation
-"Results_full_rg"
